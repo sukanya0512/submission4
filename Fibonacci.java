7bc3a76f99b66a49157db62b64c0e3e2a9b879a6
sukanya0512 <sukanyamulimani05@gmail.com>
2025-05-27 20:54:58 +0530

    save

---
+++ Fibonacci.java
@@ -0,0 +1,18 @@
+public class Fibonacci {
+
+    public static void generateFibonacci(int n) {
+        System.out.println("Fibonacci Series up to " + n + " terms:");
+        int first = 0, second = 1;
+
+        for (int i = 1; i <= n; ++i) {
+            System.out.print(first + " ");
+            int next = first + second;
+            first = second;
+            second = next;
+        }
+    }
+    public static void main (String[] args) {
+        int numberOfTerms = 10;  
+        generateFibonacci(numberOfTerms);
+    }
+}
